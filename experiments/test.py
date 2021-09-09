b50f1ab471e8b8655987c7d8eee9f3adbfaf900f
polariche <polariche@gmail.com>
2021-09-09 05:16:02 +0000

    fix errors

--- experiments/test.py
+++ experiments/test.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     sdf = analytic_sdf.SphereSDF(torch.zeros((3)), 1)
 
     # create samples
-    uv = sampler.grid_2d(64, 64).requires_grad_(True)
-    depth = torch.zeros((*uv.shape[:-1], 1), device=uv.device)
+    uv = sampler.grid_2d(64, 64).unsqueeze(0).requires_grad_(True)
+    depth = torch.zeros((*uv.shape[:-1], 1), device=uv.device).requires_grad_(True)
     pose = torch.zeros((4,4), device=uv.device)
     pose[:3, :3] = torch.eye(3)
     pose[2,3] = -2
