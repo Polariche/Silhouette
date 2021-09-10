4af888a5b4a0cf9492c2749395ee3732aa071923
polariche <polariche@gmail.com>
2021-09-10 08:35:50 +0000

    add deepsdf

---
+++ sdf.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import utils
+
+class SphereSDF(nn.Module):
+    def __init__(self, pos, r):
+        super(SphereSDF, self).__init__()
+        self.pos = pos
+        self.r = r
+    
+    def forward(self, x):
+        assert x.shape[-1] == self.pos.shape[-1]
+
+        pos = utils.sync_end_dim(self.pos, x)
+        x = x - pos
+        s = torch.sqrt((x**2).sum(dim=-1, keepdim=True)) - self.r
+
+        return s
+        
+
+# modified implementation
+class DeepSDF(nn.Module):
+    def __init__(self, num_layers):
+        super(DeepSDF, self).__init__()
+        self.num_layers = num_layers
+
+        for i in range(num_layers):
+            in_dim = 512
+            out_dim = 512
+
+            if i == 0:
+                in_dim = 3+256  #259
+            elif i == num_layers-1:
+                out_dim = 1
+            elif i == int(num_layers/2)-1:
+                out_dim = 512 - (3+256) #259
+
+            # weight-normalized linear FC
+            setattr(self, "fc" + str(i), nn.utils.weight_norm(nn.Linear(in_dim, out_dim)))
+
+            # weight normalization??
+            setattr(self, "bn" + str(i), nn.LayerNorm(out_dim))
+
+        self.dropout = nn.Dropout(0.1)
+
+        for p in self.parameters():
+            if len(p.shape) == 1:
+                nn.init.uniform(p, 0., 0.)
+
+    def forward(self, x):
+        num_layers = self.num_layers
+        x_origin = x
+
+        for i in range(num_layers):
+            fc = getattr(self, "fc" + str(i))
+            bn = getattr(self, "bn" + str(i))
+
+            if i == int(num_layers/2):
+                x = torch.cat([x, x_origin], dim=-1)
+
+            x = fc(x)
+
+            if i < self.num_layers:
+                x = F.leaky_relu(x) #torch.tanh(x)
+                x = self.dropout(x)
+
+            
+        output = F.leaky_relu(x) #torch.tanh(x)
+        return output
